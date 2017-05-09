6fb00daaac3f2ea7c68a2a17ab42ead928044c83
Raghuram <raghuram9758@gmail.com>
2017-05-09 18:30:33 +0530

    changes to delete and add groups

--- gui/utils/delete_groups.php
+++ gui/utils/delete_groups.php
@@ -1,10 +1,12 @@
 <?php
 include('dblinker.php');
 try{
-	$obj = json_decode($_POST["x"], false);
+	//$obj = json_decode($_POST["x"], false);
 	
-	$stmt = $conn->prepare("DELETE FROM signalgroupss WHERE signals=".$obj->signals );
+	$stmt = $conn->prepare("DELETE FROM signalgroupss WHERE signals='".$_POST["x"]."'" );
     $stmt->execute();
+	
+	echo 'success';
 	
 }
 catch(PDOException $e)
