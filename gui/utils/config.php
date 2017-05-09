d5a9a3e6c8dfe33aacf75ea77fdcf2d7a3062679
Minu <minuapple@gmail.com>
2017-05-09 19:24:54 +0530

    removed database credentials

--- gui/utils/config.php
+++ gui/utils/config.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "phpmyadmin";
+$servername = "servername";
+$username = "username";
 $password = "password";
-$dbname = "itspe";
+$dbname = "dbname";
 ?>
