dd648a9d647b77d0efc5128a01fe4977f90ff65d
Raghuram <raghuram9758@gmail.com>
2017-05-10 12:22:44 +0530

    updated update marker

--- gui/utils/update.php
+++ gui/utils/update.php
@@ -9,26 +9,34 @@ try{
     $stmt->bindParam(':lat', $lat);
     $stmt->bindParam(':lon', $lon);
 
-    // insert a row
     $name = $obj->name;
     $lat = $obj->lat;
     $lon = $obj->lon;
     $stmt->execute();
+	
+	$stmt = $conn->query("SELECT * FROM signalgroups");
+	while($data = $stmt->fetch(PDO::FETCH_ASSOC)){
+		$id = explode(",", $data['signals']);
+		$name = explode(",", $data['signalnames']);
+		for($i=0;$i<sizeof($id);$i++){
+			if($id[$i] == $obj->id){
+				$name[$i]=$obj->name;
+				
+				
+				//$stmt2 = $conn->prepare("DELETE FROM signalgroups WHERE id=".$data['id'] );
+				//$stmt2->execute();
+			}		
+		}
+		$data['signalnames']=implode(',',$name);
+		$stmt2 = $conn->prepare("UPDATE signalgroups SET signalnames = :signalnames WHERE id = ".$data['id']);
+		$stmt2->bindParam(':signalnames', $data['signalnames']);
+		$stmt2->execute();
+	}
+	
+	
 	echo $obj->id;
 	
 	
-	// $stmt = $conn->prepare("UPDATE signals SET name = :name,lat = :lat,lon = :lon WHERE id = 1" );
- 
-    // $stmt->bindParam(':name', $name);
-    // $stmt->bindParam(':lat', $lat);
-    // $stmt->bindParam(':lon', $lon);
-
-    // // insert a row
-    // $name = "Raghuram";
-    // $lat = "0.001";
-    // $lon = "0.01";
-    // $stmt->execute();
-	// echo "success";
 }
 catch(PDOException $e)
 {
